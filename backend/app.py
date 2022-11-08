7372977d4afab67a5cd4f958b75a8e6d9d8555db
Vlad <zerodivideone@gmail.com>
2022-11-08 23:15:22 +0200

    added some routes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import os
 import configparser
+import Moodie.backend.database.db as DB
 
 config = configparser.ConfigParser()
 #print(os.path.abspath(os.path.join("data.ini")))
@@ -11,6 +12,37 @@ app = Flask(__name__)
 @app.route("/")
 def sample():
    return "Hello, backend!"
+
+@app.route('/id/<id>', methods=['GET'])
+def api_get_movie_by_id(id):
+       movie = DB.get_movie(id)
+       
+       return jsonify(
+            {
+                "movie": movie,
+            }
+        )
+       
+@app.route('/remove', methods=["DELETE"])
+def api_remove_movie_from_user():
+       id = request.args.get('id')
+       user = request.args.get('user')
+       
+       return DB.remove_movie_from_user(id,user)
+    
+@app.route('/rating/<value>', methods=["POST"])
+def add_rating(rating):
+       id = request.args.get('id')
+       user = request.args.get('user')
+       
+       return DB.update_rating(rating, id, user)
+
+@app.route('/rating/<value>', methods=["PUT"])
+def update_rating(rating):
+       id = request.args.get('id')
+       user = request.args.get('user')
+       
+       return DB.update_rating(rating, id, user)
   
 if __name__ == '__main__':
    app.config['DEBUG'] = True
