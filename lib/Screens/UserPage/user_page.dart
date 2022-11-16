e80d62e828b8ad6bf509f0ac70284c33a1ec1414
Daniela Livia Rednic <rednic.dana20@gmail.com>
2022-11-16 16:11:40 +0200

    Added User page

---
+++ lib/Screens/UserPage/user_page.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/constants.dart';
+import 'package:flutter_auth/responsive.dart';
+
+class UserPage extends StatelessWidget {
+
+}
