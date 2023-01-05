817d25da6b73f844acf5953d21fd04b88f27a098
Vlad <zerodivideone@gmail.com>
2023-01-05 17:06:10 +0200

    made needed changes to functions

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 import os
 import configparser
-import Moodie.backend.database.db as DB
+import database.db as DB
 
 config = configparser.ConfigParser()
 #print(os.path.abspath(os.path.join("data.ini")))
@@ -13,91 +13,130 @@ app = Flask(__name__)
 def sample():
    return "Hello, backend!"
 
-
-
-@app.route('/id/<id>', methods=['GET'])
-def api_get_movie_by_id(id):
+@app.route('/movies', methods=['GET'])
+def api_get_movie_by_id(given_id=None):
+       if given_id==None:
+              id = request.args.get("id")
+       else:
+              id= given_id
        movie = DB.get_movie(id)
        
        return jsonify(
             {
-                "movie": movie,
+                "title": movie["name"],
+                "genre": movie["genre"],
+                "year": movie["year"],
+                "duration": movie["duration"],
+                "rating": movie["rating"],
+                "description": movie["description"],
+                "trailer": movie["trailer"],
+                "poster": movie["poster"],
+                "mood": movie["mood"]
             }
         )
 
 @app.route('/user/add', methods=["POST"])
 def api_add_user():
-       user = request.args.get('user')
-       passw = request.args.get('pass')
-       email = request.args.get('email')
+       user = request.form.get('user')
+       passw = request.form.get('pass').encode("UTF-8")
+       email = request.form.get('email')
+       
+       print(DB.search_user(user))
+       if (DB.search_user(user)):
+              return jsonify({"error": "User already exists",
+                              "return": True})
+       
+       if(DB.search_email(email)):
+              return jsonify({"error": "Email already in use",
+                              "return": False})
        
        return DB.add_user(user, passw, email)
 
+@app.route('/user/verify')
+def api_verify_user():
+       username = request.form.get('user')
+       passw = request.form.get('pass').encode("UTF-8")
+       
+       user = DB.search_user(username)
+       
+       if(user):
+              if(user['username'] == username and user['password'] == passw):
+                     return jsonify({"return":True})
+              else:
+                     return jsonify({"error": "User or password incorrect",
+                            "return": False})
+       
+       return jsonify({"error": "User not found",
+                            "return": False})
+
 @app.route('/user/movie/add', methods=["POST"])
 def api_add_movie_to_user():
-       id = request.args.get('id')
-       user = request.args.get('user')
+       id = request.form.get('id')
+       user = request.form.get('user')
        
        return DB.add_movie_to_user(id,user)
        
 @app.route('/user/movie/delete', methods=["DELETE"])
 def api_remove_movie_from_user():
-       id = request.args.get('id')
-       user = request.args.get('user')
+       id = request.form.get('id')
+       user = request.form.get('user')
        
        return DB.remove_movie_from_user(id,user)
 
 @app.route('/movies/add', methods=["POST"])
 def add_movie_to_db():
-       id = request.args.get('id')
-       name = request.args.get('name')
-       genre = request.args.get('genre')
-       year = request.args.get('year')
-       duration = request.args.get('duration')
-       moods = request.args.get('moods')
-       rt_rating = request.args.get('rt_rating')
-       imdb_rating = request.args.get('imdb_rating')
-       desc = request.args.get('desc')
-       trailer_link = request.args.get('trailer_link')
-       poster_link = request.args.get('poster_link')
+       id = request.form.get('id')
+       name = request.form.get('name')
+       genre = request.form.get('genre')
+       year = request.form.get('year')
+       duration = request.form.get('duration')
+       moods = request.form.get('moods')
+       rt_rating = request.form.get('rt_rating')
+       imdb_rating = request.form.get('imdb_rating')
+       desc = request.form.get('desc')
+       trailer_link = request.form.get('trailer_link')
+       poster_link = request.form.get('poster_link')
        return DB.add_movie(id,name,genre,year,duration,moods,rt_rating,imdb_rating, desc,trailer_link,poster_link)
 
 @app.route('/movies/rand', methods=["GET"])
 def api_get_random_movie():
-       genre = request.args.get('genre')
-       year = request.args.get('year')
-       mood = request.args.get('mood')
-       rating = request.args.get('rating')
+       genre = request.form.get('genre')
+       year = request.form.get('year')
+       mood = request.form.get('moods')
+       rating = request.form.get('rating')
        
-       filters = []
+       filters = {}
        filters['genre'] = genre
        filters['year'] = year
-       filters['mood'] = mood
-       filters['rating'] = rating
+       filters['moods'] = mood
+       filters['rating'] = int(rating)
        
-       movie = DB.get_random_movie(filters)
+       result = DB.get_random_movie(filters)
        
-       return jsonify(
-            {
-                "movie": movie,
-            }
-        )
+       for movie in result:
+              movie_id= movie['movie_id']
+       
+       return api_get_movie_by_id(movie_id)
+       
     
-@app.route('/rating/<value>', methods=["POST"])
-def add_rating(rating):
-       movie_id = request.args.get('id')
-       user = request.args.get('user')
+@app.route('/rating', methods=["POST","PUT"])
+def add_rating():
+       rating = request.args.get("value")
+       movie_id = request.form.get('id')
+       user = request.form.get('user')
        
        return DB.update_rating(rating, movie_id, user)
 
-@app.route('/rating/<value>', methods=["PUT"])
-def update_rating(rating):
-       movie_id = request.args.get('id')
-       user = request.args.get('user')
+# @app.route('/rating', methods=["PUT"])
+# def update_rating():
+#        rating = request.args.get("value")
+#        movie_id = request.form.get('id')
+#        user = request.form.get('user')
        
-       return DB.update_rating(rating, movie_id, user)
+#        return DB.update_rating(rating, movie_id, user)
   
 if __name__ == '__main__':
    app.config['DEBUG'] = True
    app.config['MONGO_URI'] = config['PROD']['DB_URI']
+   print(app.config['MONGO_URI'])
    app.run(host="0.0.0.0")
--- backend/database/db.py
+++ backend/database/db.py
@@ -1,4 +1,5 @@
-from flask import current_app, g
+from flask import current_app, jsonify
+from flask_pymongo.wrappers import MongoClient
 from flask_pymongo import PyMongo
 from werkzeug.local import LocalProxy
 
@@ -6,19 +7,32 @@ def get_db():
     """
     Configuration method to return db instance
     """
-    db = getattr(g, "_database", None)
-
-    if db is None:
-
-        db = g._database = PyMongo(current_app).db
-       
+    client = MongoClient(current_app.config.get("MONGO_URI", None))
+    db= client.msa
     return db
 
 db = LocalProxy(get_db)
 
 def add_user(username, password, email):
-    user = {}
-    return db.users.insert_one(user)
+    user = {"username": username,
+            "password": password,
+            "email": email,
+            "movies":[]}
+    db.users.insert_one(user)
+    return jsonify({"return": True})
+
+def search_user(username, password = None):
+    if password is None:
+        query = {"username": username}
+    else:
+        query = {"username": username, "password": password}
+
+    return db.users.find_one(query)
+
+def search_email(email):
+    query ={"email": email}
+    
+    return db.users.find_one(query)
 
 def add_movie(id,name,genre,year,duration,moods,rt_rating,imdb_rating,desc,trailer_link,poster_link):
     movie = { "movie_id" : id,
@@ -26,29 +40,34 @@ def add_movie(id,name,genre,year,duration,moods,rt_rating,imdb_rating,desc,trail
             "genre" : genre,
             "year" : year,
             "duration" : duration,
-            "rating" : (rt_rating+imdb_rating)/2,
+            "rating" : (int(rt_rating)+float(imdb_rating)*10)/2,
             "description" : desc,
             "trailer" : trailer_link,
             "poster" : poster_link,
             "mood" : moods
             }
-    return db.movies.insert_one(movie)
+    db.movies.insert_one(movie)
+    
+    return jsonify({"return": True})
 
 def aggregate_filters(filters):
     query={}
     
     if "genre" in filters:
-        query["genre"] = { "$in": filters["genre"] }
+        genre=[filters["genre"]]
+        query["genre"] = { "$in": genre }
     
-    if "year" in filters:
-        query["year"] = filters["year"]
+    # if "year" in filters:
+    #     query["year"] = filters["year"]
     
-    if "mood" in filters:
-        query["mood"] = { "$in": filters["moods"] }
+    # if "mood" in filters:
+    #     mood=[filters["moods"]]
+    #     query["mood"] = { "$in": mood }
     
-    if "rating" in filters:
-        query["rating"] = { "$gte": filters["rating"] }
+    # if "rating" in filters:
+    #     query["rating"] = { "$gte": filters["rating"] }
     
+    print(query)
     return db.movies.aggregate([
         { "$match": query },
         { "$sample": { "size": 1 } } 
@@ -62,34 +81,37 @@ def get_random_movie(filters):
     return movie
 
 def get_movie(id):
-    query_movie = { "id" : id}
-    return db.movies.find_one(query_movie, {})
+    query_movie = { "movie_id" : id}
+    return db.movies.find_one(query_movie)
 
 def add_movie_to_user(movie_id,username):
-    query_user = { "Username" : username}
-    response = db.users.update_one(query_user, 
-                { "$push": 
-                    { "movies":
-                        {
-                            {"movie_id": movie_id,
-                             "rating" : -1}
-                        }
-                    } 
-                } )
-    
-    return response
+    query_user = { "username" : username}
+    db.users.update_one(query_user, 
+                        { "$push": 
+                            { "movies":
+                                {"movie_id": movie_id,
+                                "rating" : -1}
+                            } 
+                        } )
+    return jsonify({"return": True})
 
 def update_rating(rating,movie_id,username):
-    query_user = { "Username" : username, "movies.movie_id":movie_id}
+    query_user = { "username" : username, "movies.movie_id":movie_id}
+    print(db.users.find_one(query_user))
     response = db.users.update_one(query_user, 
                 { "$set": 
-                    { "movies.$.rating": rating } 
-                } )
+                    { "movies":
+                        {
+                            "movie_id": movie_id,
+                            "rating": rating 
+                        } 
+                    } 
+                })
     
-    return response
+    return jsonify({"return": True})
 
 def remove_movie_from_user(movie_id,username):
-    query_user = { "Username" : username}
+    query_user = { "username" : username}
     response = db.users.update_one(query_user, 
                 { "$pull":{
                     "movies":{
@@ -98,4 +120,4 @@ def remove_movie_from_user(movie_id,username):
                     }
                 } )
     
-    return response
+    return jsonify({"return": True})
